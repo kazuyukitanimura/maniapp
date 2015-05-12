d81ffdea3bd7fd2014a3e3895fe8ffc0e2ebe7cc
kazuyukitanimura <kazuyukitanimura@gmail.com>
2015-05-12 01:41:49 -0700

    menu login

--- referralio/MenuViewController.swift
+++ referralio/MenuViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class MenuViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = AppColors.Black
+    let loginButton = FBSDKLoginButton()
+    let constrainedViews = ConstrainedViews(views: ["loginButton": loginButton], formats:["H:|-(>=16)-[loginButton]-16-|", "V:|-64-[loginButton]-(>=32)-|"])
+    view.addConstrainedViews(constrainedViews)
   }
 }
